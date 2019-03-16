cd409171083b71c5b0b4f37770299f4cc025180e
Steven Hale <elah.nevets@gmail.com>
2019-03-16 22:48:37 +0100

    add comments

--- e2g.py
+++ e2g.py
@@ -13,6 +13,9 @@ def parseargs():
 	return args
 
 def time_convert(latitude, longitude, t, tf):
+	""" 
+	Latitude and longitude are used to determine timezone. The correct day local time can then be determined.
+	"""
 	e = datetime.strptime(t, '%Y%m%dT%H%M%SZ')
 	e.astimezone(timezone(tf.timezone_at( lat=latitude, lng=longitude)))
 	return e.strftime('%Y%m%d')
@@ -21,16 +24,23 @@ def run(args):
 	out = open(args.out_file, "w")
 	c = Calendar.from_ical(open(args.in_file).read())
 	tf = TimezoneFinder()
-	for component in c.walk():
-		if component.name == "VEVENT":
-			latitude = float(component.get('geo').to_ical().split(";")[0])
-			longitude = float(component.get('geo').to_ical().split(";")[1])
-			component['location'] = " ".join(component.get('geo').to_ical().split(";"))
-			component['description'] = component.get('url') + " " + args.tags
-			component['dtstart'] = time_convert(latitude, longitude,component['dtstart'].to_ical().decode('utf8') , tf)
-			component['dtend'] = component['dtstart']
-			print(component['summary'] )
+	googleify(c, args, tf)
 	out.write(c.to_ical().decode('UTF-8').replace('\r\n', '\n').strip())
+
+def googleify(c, args, tf):
+	"""
+	Converts eventor geo to location which is used by google. eventor supplied url and tags (supplied as args) are used to create description.
+	Events are converted to all day events to avoid confusion caused by different time zones. 
+	"""
+    for component in c.walk():
+            if component.name == "VEVENT":
+                    latitude = float(component.get('geo').to_ical().split(";")[0])
+                    longitude = float(component.get('geo').to_ical().split(";")[1])
+                    component['location'] = " ".join(component.get('geo').to_ical().split(";"))
+                    component['description'] = component.get('url') + " " + args.tags
+                    component['dtstart'] = time_convert(latitude, longitude,component['dtstart'].to_ical().decode('utf8') , tf)
+                    component['dtend'] = component['dtstart']
+                    print(component['summary'] )
 
 if __name__ == '__main__':
 	args = parseargs()
